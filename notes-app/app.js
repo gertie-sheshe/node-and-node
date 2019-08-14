7bf0083591e975f49a7b1145c8c2ec6b3e49f68c
gertie-sheshe <gertrude.nyenyeshi@andela.com>
2019-08-14 12:53:33 +0300

    register add, remove, list yargs commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,42 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+// Register add command
+yargs.command({
+    command: 'add',
+    description: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+});
 
-if (command === 'add') {
-    console.log('Adding note')
-} else if (command === 'remove') {
-    console.log('Remoning note')
-}
+// Register remove command
+yargs.command({
+    command: 'remove',
+    description: 'Removing note',
+    handler: function() {
+        console.log('Removing that note');
+    }
+});
+
+// Register list command
+yargs.command({
+    command: 'list',
+    description: 'Listing all notes',
+    handler: function() {
+        console.log('Listing all notes');
+    }
+});
+
+// Register read command
+yargs.command({
+    command: 'read',
+    description: 'Reading notes',
+    handler: function() {
+        console.log('Reading a note');
+    }
+});
+
+console.log(yargs.argv)
